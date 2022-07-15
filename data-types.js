a2c3d4d5cd1ddab3efc0677f8210cba94463fc00
Sadman95 <mail.sakib.1995@gmail.com>
2022-07-16 00:46:49 +0600

    discussion on primitive data types

---
+++ data-types.js
@@ -0,0 +1,16 @@
+let name = "sifat";
+let age = 22;
+
+let isLearning = true;
+let isMarried = false;
+
+let havePet;
+
+let haveBiryani = null;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLearning);
+console.log(typeof isMarried);
+console.log(typeof havePet);
+console.log(typeof haveBiryani);
---
+++ data-types.md
@@ -0,0 +1,11 @@
+# Data types
+
+- Primitive type
+  - string
+  - number
+  - boolean
+  - undefined(special)
+  - null(special)
+- Object type
+  - object
+  - array
