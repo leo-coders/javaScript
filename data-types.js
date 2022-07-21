f39a17ba3064a3fa93b051f62c217750e19cbb7a
Sadman95 <mail.sakib.1995@gmail.com>
2022-07-22 01:16:34 +0600

    discussion - object type

--- data-types.js
+++ data-types.js
@@ -1,3 +1,6 @@
+/* -------------------
+PRIMITIVE TYPES
+ ---------------------*/
 let name = "sifat";
 let age = 22;
 
@@ -14,3 +17,42 @@ console.log(typeof isLearning);
 console.log(typeof isMarried);
 console.log(typeof havePet);
 console.log(typeof haveBiryani);
+
+/* ---------------------
+OBJECT
+---------------------- */
+const accesories = {
+  // key : value
+  laptop: true,
+  pen: "matador",
+  phone: "mi",
+  watch: "hyolu",
+  book: "animal firm",
+  phonePrice: 20000,
+};
+accesories.laptop = "dell";
+
+accesories.adapter = "sandisk";
+accesories.hardDisk = "adata";
+accesories.phone = "samsung";
+
+/**
+ * case -> error(assignment to constant variable)
+ * case -> no error(object is declared by let)
+ */
+/* accesories = {
+  notebook: "basundara",
+  earphone: "xiaomi",
+}; */
+
+// console.log(typeof accesories);
+
+// console.log(accesories);
+
+const product = {
+  name: "laptop",
+  brand: "hp",
+  description:
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, reiciendis.",
+  price: 32000,
+};
