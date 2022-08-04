448783ad0afc539b45d829b30dda7fed5f2f3ed5
Sadman Sakib <86881892+Sadman95@users.noreply.github.com>
2022-08-04 23:54:36 +0600

    Delete loop.js

--- loop.js
+++
@@ -1,9 +0,0 @@
-/* for (let plateServe = 0; plateServe < 10; plateServe++) {
-  console.log("plate served");
-} */
-
-let ticketServe = 10;
-while (ticketServe > 0) {
-  console.log("ticketServe-", ticketServe);
-  ticketServe--;
-}
